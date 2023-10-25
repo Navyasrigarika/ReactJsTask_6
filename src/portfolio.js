f278894e2d1d5619c5f07fbcb5f122214dee6003
Navyasrigarika <144647488+Navyasrigarika@users.noreply.github.com>
2023-10-25 10:51:42 +0530

    Add files via upload

---
+++ src/portfolio.js
@@ -0,0 +1,63 @@
+const header = {
+    homepage: 'https://navyasri.github.io/cleanfolio',
+    title: 'Hello!',
+  }
+  
+  const about = {
+    name: 'Navya sri',
+    role: 'Computer Science Engineer',
+    description:
+      'Currently pursuing my B.tech 3rd year at VIT AP University.',
+    resume: 'https://example.com',
+    social: {
+      linkedin: 'https://www.linkedin.com/in/garika-navyasri-05994625b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+      github: 'https://github.com/Navyasrigarika',
+    },
+  }
+  
+  const projects = [
+   
+    {
+      name: 'Project 1',
+      description:
+        'Bird Species Prediction Using Recurrent Neural Networks (RNN) ',
+      stack: ['ML', 'Rnn', 'React'],
+      sourceCode: 'https://github.com',
+      livePreview: 'https://github.com',
+    },
+    {
+      name: 'Project 2',
+      description:
+        'Lie Ditector',
+      stack: ['Arduino', 'c++', 'React'],
+      sourceCode: 'https://github.com',
+      livePreview: 'https://github.com',
+    },
+    {
+      name: 'Project 3',
+      description:
+        'Reasearch Paper on Cyber crime',
+      stack: ['SASS', 'TypeScript', 'React'],
+      sourceCode: 'https://github.com',
+      livePreview: 'https://github.com',
+    },
+  ]
+  
+  const skills = [
+    
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Git',
+    'Sql',
+    'Java',
+    'Python'
+  ]
+  
+  const contact = {
+    email: 'navyasrigarika49@gmail.com',
+  }
+  
+  export { header, about, projects, skills, contact }
+  
